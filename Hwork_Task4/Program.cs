05f36cf52528754262da269ca6426be1ee5a3421
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-04-26 15:08:50 +0600

    add Hwork_Task4

---
+++ Hwork_Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 4: Напишите программу, которая на вход 
+// принимает натуральное число N, а на выходе показывает 
+// его цифры через запятую.
+
+
+Console.WriteLine("Input your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("The digits of the number {num} are separated by commas: ");
+
+string numStr = num.ToString();
+Console.Write(numStr[0]);
+
+for (int i = 1; i < numStr.Length; i++)
+{
+    Console.Write($",{numStr[i]}");
+}
+Console.WriteLine();

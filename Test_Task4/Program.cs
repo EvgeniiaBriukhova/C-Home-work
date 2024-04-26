07e6a21c50ccb7bc1dce93a74ee13ef16be65b10
Evgeniia Briukhova <eve.briukhova897@gmail.com>
2024-04-26 15:30:39 +0600

    add Test for Task4

---
+++ Test_Task4/Program.cs
@@ -0,0 +1,53 @@
+﻿//вводит натуральное числоб пишет его в строку и переворачивает.
+
+// Console.Write("Input number N: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+
+// if (N < 10)
+// {
+// Console.WriteLine(N);
+// }
+// else
+// {
+// while (N > 0)
+// {
+// int currentDigit = N % 10;
+// N /= 10;
+// if (N > 0)
+// {
+// Console.Write(currentDigit + ",");
+// }
+// else
+// {
+// Console.WriteLine(currentDigit);
+// }
+// }
+// }
+
+//Вводит числобпишет его в строку через запятую.
+
+Console.Write("Input number N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+if (N < 0)
+{
+    Console.WriteLine("Input error - numer should be positive.");
+}
+else if (N < 10)
+{
+    Console.WriteLine(N);
+}
+else
+{
+    string numStr = N.ToString();
+    Console.Write(numStr[0]); // Выводим первую цифрим последнюю цифру
+
+
+// Выводим остальные цифры в обратном порядке, разделяя их запятой
+    for (int i = 1; i < numStr.Length; i++)
+    {
+        Console.Write($",{numStr[i]}");
+    }
+
+    Console.WriteLine();
+}
